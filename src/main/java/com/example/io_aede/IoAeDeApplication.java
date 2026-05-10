b94401d483e9b744b10963218ffc61e5b1d81c34
unknown <tomaszhamerla@interia.pl>
2026-05-10 08:48:04 +0200

    init

---
+++ src/main/java/com/example/io_aede/IoAeDeApplication.java
@@ -0,0 +1,13 @@
+package com.example.io_aede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoAeDeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoAeDeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/io_aede/IoAeDeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.io_aede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoAeDeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
